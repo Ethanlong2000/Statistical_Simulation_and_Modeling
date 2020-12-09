56fa5fc2b25e291488e1a4903dba48cb14202bce
Ethanlong2000 <65759079+Ethanlong2000@users.noreply.github.com>
2020-12-09 09:46:36 +0800

    作业1

--- week15.R
+++ week15.R
@@ -2,18 +2,26 @@
 # Objective : TODO
 # Created by: lyh
 # Created on: 2020/12/9
-x=c(6,7,3,4,7,3,7,2,6,3,7,8,2,1,3,5,8,7)
-n=length(x)
-N=0
-m=8
-for (i in 1:(m+1)){
-  N[i]=length(x[x==i-1])
-}
-p_hat=mean(x)/m
-p=0
-p[1]=pbinom(0,m,p_hat)
-for (i in 2:(m+1)){
-  p[i]=pbinom(i-1,m,p_hat)-pbinom(i-2,m,p_hat)
-}
-Q=sum((N-n*p)^2/(n*p))
-pvalue=1-pchisq(Q,m)
+
+#x=c(6,7,3,4,7,3,7,2,6,3,7,8,2,1,3,5,8,7)#pvalue=1-pchisq(Q,m)
+#homework1
+#N=0
+#m=8
+#Q=c()
+#for (j in 1:10000){
+#  x=rbinom(18,m,0.6180556)
+#  n=length(x)
+#  for (i in 1:(m+1)){
+#  N[i]=length(x[x==i-1])
+#  }
+# p_hat=mean(x)/m
+# p=0
+# p[1]=pbinom(0,m,p_hat)
+# for (i in 2:(m+1)){
+#  p[i]=pbinom(i-1,m,p_hat)-pbinom(i-2,m,p_hat)
+# }
+# Q[j]=sum((N-n*p)^2/(n*p))
+#}
+#P=length(Q[Q>31.4993])/length(Q)
+#P
+
